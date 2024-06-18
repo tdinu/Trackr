ab8496a97d7149e103cafb343652d4cf1c9021b3
tdinu <dinutitiana@yahoo.com>
2024-06-18 17:01:33 +0200

    Initial Commit

---
+++ Trackr/TrackrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackrApp.swift
+//  Trackr
+//
+//  Created by T Dinu on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrackrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
