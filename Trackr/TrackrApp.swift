c6f8726a12387c9331757ad954d3a0853f44a655
tdinu <dinutitiana@yahoo.com>
2024-06-18 20:55:27 +0200

    basic setup start

--- Trackr/TrackrApp.swift
+++ Trackr/TrackrApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct TrackrApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
